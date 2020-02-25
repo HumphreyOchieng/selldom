1e628b1ac8f012ade15c9f744f81f12edc0ee14b
Humphrey Ochieng <humochieng9@gmail.com>
2020-02-25 17:11:21 +0300

    Modify cart design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,19 +29,23 @@
 			</div>
 			<div class="header">
 				<div class="quantity">
-					<span style="font-weight: bold;">{{cart.length}}</span>
-					<span v-if="cart.length===1"> item</span>
-					<span v-if="cart.length!==1"> items</span>
+					<span v-if="cart.length===1" style="color:gray">Item: </span>
+					<span v-if="cart.length!==1" style="color:gray">Items: </span>
+					<span style="font-weight: bold;color:#38f;">{{cart.length}}</span>
 				</div>
-				<div class="cost">Total: <span style="font-weight: bold;">Sh. {{totalCostOfCart}}</span></div>
+				<div class="cost">
+					<span style="color:gray">Total: </span>
+					<span style="font-weight:bold;color:#38f;">Sh. {{totalCostOfCart}}</span>
+				</div>
 			</div>
-			<div class="save" v-for="(post, index) in cart" :key="index">
-				<img src="http://dummyimage.com/800x600/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image" class="image">
+			<div class="post" v-for="(post, index) in cart" :key="index">
+				<!-- <img src="http://dummyimage.com/800x600/4d494d/686a82.gif&text=placeholder+image" alt="placeholder+image" class="image"> -->
 				<div class="description">
 					<div class="title">{{post.title}}</div>
 					<div class="price">Sh. {{post.price}}</div>
 				</div>
 			</div>
+			<sui-button primary fluid large :class="checkout">Checkout</sui-button>
 		</div>
 
 		<sui-modal v-model="isProductModalVisible">
@@ -114,43 +118,6 @@ export default {
 </script>
 
 <style lang="scss">
-.home {
-	.panels {
-		padding-top: 5px;
-		position: relative;
-
-		.cart {
-			border: 1px solid gray;
-			border-radius: 3px;
-			margin-left: 10px;
-			position: absolute;
-			top: 5px;
-			right: 0;
-			background: #fff;
-			width: 400px;
-
-			.header {
-				display: flex;
-				justify-content: space-between;
-				padding: 10px 5px;
-			}
-
-			.message {
-				text-align: center;
-				padding: 2em;
-			}
-		}
-
-		.cart__open {
-			height: auto;
-		}
-		.cart__closed {
-			height: 0;
-			display: none;
-		}
-	}
-}
-
 .navigation {
 	display: flex;
 	padding: 10px 0;
@@ -163,66 +130,106 @@ export default {
 	}
 }
 
+.panels {
+	padding-top: 5px;
+	position: relative;
+}
+
 .posts {
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
 	grid-gap: 5px;
-}
 
-.post {
-	border: 1px solid gray;
-	border-radius: 3px;
+	.post {
+		border: 1px solid gray;
+		border-radius: 3px;
 
-	.image {
-		max-width: 100%;
-	}
-
-	.content {
-		padding: 5px;
-
-		.title {
-			font-weight: bold;
+		.image {
+			max-width: 100%;
 		}
 
-		.details {
-			display: flex;
-			padding-top: 5px;
+		.content {
+			padding: 5px;
 
+			.title {
+				font-weight: bold;
+			}
+
+			.details {
+				display: flex;
+				padding-top: 5px;
+
+				.price {
+					flex: 1;
+				}
+			}
+		}
+
+		&:hover {
+			cursor: pointer;
+			border: 1px solid black;
+		}
+	}
+}
+
+.cart {
+	border: 1px solid gray;
+	border-radius: 3px;
+	margin-left: 10px;
+	position: absolute;
+	top: 5px;
+	right: 0;
+	background: #fff;
+	width: 350px;
+	padding: 5px;
+
+	.header {
+		display: flex;
+		justify-content: space-between;
+		padding: 10px 5px;
+		border-bottom: 1px solid gray;
+	}
+
+	.message {
+		text-align: center;
+		padding: 2em;
+	}
+
+	.post {
+		display: flex;
+		padding: 5px;
+
+		.image {
+			max-width: 30%;
+		}
+
+		.description {
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			padding-left: 10px;
+
+			.title {
+				font-weight: bold;
+				padding-bottom: 5px;
+			}
 			.price {
-				flex: 1;
+				margin-bottom: auto;
 			}
 		}
 	}
 
-	&:hover {
-		cursor: pointer;
-		border: 1px solid black;
+	.checkout {
+		margin-top: 10px;
 	}
 }
 
-.save {
-	display: flex;
-	padding: 5px;
-	border-top: 1px solid gray;
-
-	.image {
-		max-width: 30%;
-	}
-
-	.description {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding-left: 10px;
-
-		.title {
-			font-weight: bold;
-			padding-bottom: 5px;
-		}
-		.price {
-			margin-bottom: auto;
-		}
-	}
+.cart__open {
+	height: auto;
+}
+.cart__closed {
+	height: 0;
+	display: none;
 }
 
 .modal {
